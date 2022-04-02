8550737ba9bb8c2db5032d51f3f0ced2bf6ef577
Yaprak Yıldırım <mrsyaprakyildirim@gmail.com>
2022-04-03 00:45:56 +0300

    Added Admin Pannel and category crud operations ok

---
+++ technologyMarket.EntityLayer/Entitites/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace technologyMarket.EntityLayer.Entitites
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "You cannot leave the required field blank.")]
+        [Display(Name = "Name")]
+        [StringLength(50, ErrorMessage = "Must be a maximum of 50 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "You cannot leave the required field blank.")]
+        [Display(Name = "Description")]
+        [StringLength(150, ErrorMessage = "Must be a maximum of 150 characters.")]
+        public string Description { get; set; }
+
+        //Bir kategoride birden fazla ürün alabilir
+        public virtual List<Product> Products {get; set;}
+    }
+}
---
+++ technologyMarket/Controllers/AdminCategoryController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using technologyMarket.BusinessLayer.Concrete;
+using technologyMarket.EntityLayer.Entitites;
+
+namespace technologyMarket.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategory
+        CategoryRepository categoryRepository = new CategoryRepository();
+        public ActionResult Index()
+        {
+            return View(categoryRepository.List());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Create(Category p)
+        {
+            if (ModelState.IsValid)
+            {
+                categoryRepository.Insert(p);
+
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Something went wrong");
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var delete = categoryRepository.GetById(id);
+            categoryRepository.Delete(delete);
+            return RedirectToAction("Index");
+        }
+
+       public ActionResult Update(int id)
+        {
+            var update = categoryRepository.GetById(id);
+            return View(update);
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult Update(Category p)
+        {
+            if (ModelState.IsValid)
+            {
+                var update = categoryRepository.GetById(p.Id);
+                update.Name = p.Name;
+                update.Description = p.Description;
+                categoryRepository.Update(update);
+                return RedirectToAction("Index");
+            }
+            ModelState.AddModelError("", "Something went wrong");
+            return View();
+        }
+    }
+}
