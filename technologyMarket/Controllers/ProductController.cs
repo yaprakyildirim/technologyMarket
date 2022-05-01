e31c76139f97fd35d459a8fe96de9ca273ab3380
Yaprak Yıldırım <mrsyaprakyildirim@gmail.com>
2022-05-01 17:50:45 +0300

    User login, logout, sign OK

---
+++ technologyMarket/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using technologyMarket.BusinessLayer.Concrete;
+
+namespace technologyMarket.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ProductRepository productRepository = new ProductRepository();
+        public PartialViewResult PopularProduct()
+        {
+            var product = productRepository.GetPopularProduct();
+            ViewBag.popular = product;
+            return PartialView();
+        }
+    }
+}
