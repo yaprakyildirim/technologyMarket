607888d3a632a86cc0bbe50e2775b9ac4282a3d6
Yaprak Yıldırım <mrsyaprakyildirim@gmail.com>
2022-05-25 02:25:45 +0300

    After sales operations and critic stocks count for admin panel

---
+++ technologyMarket/Controllers/AdminSalesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using PagedList.Mvc;
+using technologyMarket.DataAccessLayer.Context;
+
+namespace technologyMarket.Controllers
+{
+    public class AdminSalesController : Controller
+    {
+        // GET: AdminSales
+        DataContext db = new DataContext();
+        public ActionResult Index(int sayfa=1)
+        {
+            return View(db.Sales.ToList().ToPagedList(sayfa,5));
+        }
+    }
+}
